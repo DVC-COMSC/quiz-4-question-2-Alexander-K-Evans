9446b7b0b533bc665531a804ec4ce5892deaf89d
Alexander Evans <kingevans22202@gmail.com>
2022-10-03 15:13:49 -0700

    q2

--- q2.py
+++ q2.py
@@ -1,4 +1,19 @@
+strings = []
+s = 'a'
+while s != 'stop' and s!= 'Stop':
+    s = input()
+    strings.append(s)
+    
 
+short = 0
+long = 0
+for i in range(len(strings)):
+    if len(strings[i]) > len(strings[long]):
+        long = i
+    if len(strings[i]) < len(strings[short]):
+        short = i
+
+print(strings[long], strings[short])
 # ******************************
 # Make your Code
 # ******************************
